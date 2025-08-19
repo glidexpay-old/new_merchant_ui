16d25b3cba137be04f4387cd6d50246fa0d70cbc
John Doe <johndoe@example.com>
2025-08-19 17:20:23 +0000

    add package

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   images: {
+    unoptimized: true,
     domains: ['via.placeholder.com'],
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://dashboard.glidexpay.com' : '',
 };
 
 module.exports = nextConfig;
